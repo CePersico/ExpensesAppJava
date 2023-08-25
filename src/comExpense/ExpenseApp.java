8f4bee7c79a33a3823676beb6332b38a2b04fd1c
cecil <cepersico.3@gmail.com>
2023-08-25 20:40:24 +0200

    implementando JOption

--- src/comExpense/ExpenseApp.java
+++ src/comExpense/ExpenseApp.java
@@ -10,6 +10,7 @@ import comExpense.interfaces.ExpenseCalculator;
 import comExpense.implem.ExpenseCalculatorImplementation;
 import comExpense.utils.Utilities;
 
+import javax.swing.JOptionPane;
 import java.util.*;
 
 public class ExpenseApp {
@@ -20,80 +21,81 @@ public class ExpenseApp {
 
                 ExpenseCalculator expenseCalculator = new ExpenseCalculatorImplementation();
                 ExpenseAmountValidator expenseAmountValidator = new ExpenseAmountValidatorImplementation();
-                // Declaramos un map para contar la aparición de categorias
+
                 Map<String, Integer> countCategoryMap = new HashMap<>();
-                // Refactorizamos el array a una List<Expense>
+
                 List<Expense> expenses = new ArrayList<>();
                 int index = 0;
-                int quantity = 0;
+                double quantity = 0;
                 String name;
                 String date;
-                // Definimos una variable que será definida por el usaurio para controlar
-                // la cantidad de iteraciones que hará el bucle do/while
-                int cantGastosACargar;
 
-                // Se inicializa la variable de control del do/while con lo definido por el usuario
-                System.out.print("¿Cuántos gastos desea cargar?: ");
-                cantGastosACargar = scanner.nextInt();
+                int numExpenses;
+
+
+                System.out.print("Indique la cantidad de gastos a ingresar: ");
+                //numExpenses = Integer.parseInt(JOptionPane.showInputDialog("Indique la cantidad de gastos a ingresar: " ));
+                numExpenses = scanner.nextInt();
                 do {
                         try {
-                                Expense expense = new Expense();
                                 Category category = new Category();
+                                Expense expense = new Expense();
+
                                 System.out.print("Ingrese el monto del gasto: ");
-                                quantity = scanner.nextInt();
+                                //quantity = Double.parseDouble(JOptionPane.showInputDialog( "Indique el monto del gasto: ") );
+                                quantity = scanner.nextDouble();
 
                                 if (!expenseAmountValidator.validateAmount(quantity)) {
-                                        System.out.println("El monto es válido.");
+                                        System.out.println("Monto valido");
                                 }
                                 scanner.nextLine();
 
                                 System.out.print("Ingrese la categoría del gasto: ");
-                                name = scanner.nextLine();
+                              name = scanner.nextLine();
+                              //name = JOptionPane.showInputDialog( "Indique la categoría del gasto ingresado: ");
 
                                 System.out.println("Ingrese la fecha del gasto: (dd/MM/yyyy)");
                                 date = scanner.nextLine();
+                              //date = JOptionPane.showInputDialog("Indique la fecha del gasto: (dd/MM/yyyy)");
 
-                                // Actuaizamos el map contador de categorias
+
                                 countCategoryMap.put(name, countCategoryMap.getOrDefault(name, 0) + 1);
 
                                 expense.setQuantity(quantity);
                                 category.setName(name);
                                 expense.setCategory(category);
                                 expense.setDate(date);
-                                System.out.println("Detalle del gasto ingresado: " + expense);
-                                System.out.println("Cálculo del monto del gasto ingresado: " + expenseCalculator.calculateExpense(expense));
+                            //System.out.println("Detalle del gasto ingresado: " + expense);
+                               System.out.println("Suma gastos ingresados: " + expenseCalculator.calculateExpense(expense));
+                              //JOptionPane.showMessageDialog(null,"Detalle del gasto ingresado: " + expense );
+                              //JOptionPane.showMessageDialog(null,"Cálculo del monto del gasto ingresado: " + expenseCalculator.calculateExpense(expense));
 
                                 expenses.add(expense);
                                 index++;
 
                         } catch (InvalidExpenseException exception) {
-                                System.out.println("Error" + exception);
+                              System.out.println("Error" + exception);
+                              //JOptionPane.showMessageDialog(null,"Error" + exception);
                         }
-                } while (index < cantGastosACargar);
-                System.out.println("Total del monto del gasto ingresado: " + expenseCalculator.calculateTotalExpense(expenses));
+                } while (index < numExpenses);
+              System.out.println("Total gastos ingresados: " + expenseCalculator.calculateTotalExpense(expenses));
+              //JOptionPane.showMessageDialog(null,"Total del monto del gasto ingresado: " + expenseCalculator.calculateTotalExpense(expenses));
 
-                System.out.println("DETALLE DE GASTOS INGRESADOS");
-        /* Antes de usar una forma declarativa
-        for (Expense expense : expenses) {
-            System.out.println(expense);
-        }*/
-                // Implementamos programación funcional con Stream API para recorrer la lista de gastos y mostrar solo los primeros 5
+                System.out.println("DETALLE GASTOS");
+
+
                 expenses.stream()
                         .limit(5)
                         .forEach(System.out::println);
 
-                // Recorremos el map para mostrar por consola
+
                 System.out.println("CONTADOR POR CATEGORÍA");
-        /* Antes de usar una forma declarativa
-        for (Map.Entry<String, Integer> entry : countCategoryMap.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }*/
-                // Implementamos programación funcional con Stream API para recorrer el map contador de categorias
+
                 countCategoryMap.forEach((category, count) -> System.out.println(category + ": " + count));
 
-                System.out.println();
+                System.out.println("----------------------------------------");
                 System.out.println("IMPRESIÓN DE GASTOS USANDO UN MÉTODO GENËRICO");
-                // Si tuviesemos una lista de categorías podríamos usar el mismo método
+
                 Utilities.imprimirElementos(expenses);
         }
 }
